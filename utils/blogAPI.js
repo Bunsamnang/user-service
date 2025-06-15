8b3ef3dc541ee671e0837d0d1fb19d1fab36af8a
Khut Bunsamnang <138737983+Bunsamnang@users.noreply.github.com>
2025-06-15 11:15:57 +0700

    add http:// to ep

--- utils/blogAPI.js
+++ utils/blogAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const blogAPI = axios.create({
-  baseURL: "3.83.112.120:5003", // blog-service internal URL
+  baseURL: "http://3.83.112.120:5003", // blog-service internal URL
   timeout: 5000,
 });
 
